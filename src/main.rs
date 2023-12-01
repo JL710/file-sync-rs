e988dcc07f47982b908c8bfded41233b3c076061
JL710 <76447362+JL710@users.noreply.github.com>
2023-12-01 20:41:13 +0100

    Add dirs dependency and update database path

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,22 @@ mod gui;
 mod syncing;
 use crate::gui::run;
 
+#[cfg(debug_assertions)]
+fn get_db_path() -> String {
+    String::from("development.db")
+}
+
+#[cfg(not(debug_assertions))]
+fn get_db_path() -> String {
+    let app_data_dir = dirs::data_dir().unwrap().join("file-sync-rs");
+    if !app_data_dir.is_dir() {
+        std::fs::create_dir(&app_data_dir).unwrap();
+    }
+    app_data_dir.join("data.db").to_str().unwrap().into()
+}
+
 fn main() {
-    let app_settings = db::AppSettings::new("test.db".into()).unwrap();
+    let app_settings = db::AppSettings::new(get_db_path().into()).unwrap();
 
     run(app_settings);
 }
