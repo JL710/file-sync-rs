3a19e9f5844812504aec2650890ebf000ef3b546
JL710 <76447362+JL710@users.noreply.github.com>
2023-12-01 19:37:49 +0100

    Add utils module to gui and use switch to error_popup

--- src/gui/utils.rs
+++ src/gui/utils.rs
@@ -1,9 +1,7 @@
 pub fn error_popup(prompt: &str) {
-    sync::InvalidSyncerParameters::TargetInSource(source) => {
-        rfd::MessageDialog::new()
-            .set_title("Error")
-            .set_buttons(rfd::MessageButtons::Ok)
-            .set_description(lang::target_in_source_error(lang, &source))
-            .show();
-    }
+    rfd::MessageDialog::new()
+        .set_title("Error")
+        .set_buttons(rfd::MessageButtons::Ok)
+        .set_description(prompt)
+        .show();
 } 
